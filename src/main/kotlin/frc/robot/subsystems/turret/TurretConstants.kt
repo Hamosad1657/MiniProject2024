81f5dc0b9dbcb947aee6a3d6a42e99aaf97482ac
Hamosad 1657 <hamosadr@gmail.com>
2023-11-22 18:20:09 +0200

    Shaked and Hagar: made turret subsystem

--- src/main/kotlin/frc/robot/subsystems/turret/TurretConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/turret/TurretConstants.kt
@@ -1,3 +1,13 @@
 package frc.robot.subsystems.turret
 
-object TurretConstants
+import com.ctre.phoenix.sensors.WPI_CANCoder
+import com.hamosad1657.lib.motors.HaCANSparkMax
+import com.hamosad1657.lib.motors.HaTalonFX
+import edu.wpi.first.wpilibj2.command.SubsystemBase
+import frc.robot.RobotMap
+
+object TurretConstants{
+	const val kP = 0.0
+	const val kI = 0.0
+	const val kD = 0.0
+}
--- src/main/kotlin/frc/robot/subsystems/turret/TurretSubsystem.kt
+++ src/main/kotlin/frc/robot/subsystems/turret/TurretSubsystem.kt
@@ -1,12 +1,30 @@
 package frc.robot.subsystems.turret
 
-import com.ctre.phoenix.motorcontrol.ControlMode
+import com.ctre.phoenix.sensors.AbsoluteSensorRange
 import com.ctre.phoenix.sensors.CANCoder
 import com.hamosad1657.lib.motors.HaTalonFX
+import edu.wpi.first.math.controller.PIDController
+import edu.wpi.first.math.geometry.Rotation2d
+import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.RunCommand
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import frc.robot.RobotMap
+import frc.robot.subsystems.turret.TurretConstants.kD
+import frc.robot.subsystems.turret.TurretConstants.kI
+import frc.robot.subsystems.turret.TurretConstants.kP
 
 object TurretSubsystem : SubsystemBase() {
-	private val turretMotor = HaTalonFX(RobotMap.Turret.HATALONFX_ID)
-	private val canCoder = CANCoder(RobotMap.Turret.CANCODER_ID)
+	private val motor = HaTalonFX(RobotMap.Turret.HATALONFX_ID)
+	private val encoder = CANCoder(RobotMap.Turret.CANCODER_ID)
+
+	private val turretController = PIDController(kP, kI, kD)
+
+	init {
+		encoder.configAbsoluteSensorRange(AbsoluteSensorRange.Unsigned_0_to_360)
+	}
+
+	fun getToAngleCommand(desiredAngleSupplier: () -> Rotation2d): Command {
+		val output = turretController.calculate(encoder.position, desiredAngleSupplier().degrees)
+		return RunCommand({motor.set(output)}).finallyDo { motor.set(0.0) }
+	}
 }
