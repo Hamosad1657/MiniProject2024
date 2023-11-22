939191798e7ef0d6b7927297523e44dc4b49eca7
Hamosad 1657 <hamosadr@gmail.com>
2023-11-22 18:18:51 +0200

    Shaked and Hagar: made the shootBallsCommand be a closed loop.

--- src/main/kotlin/frc/robot/subsystems/shooter/ShooterConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/shooter/ShooterConstants.kt
@@ -2,4 +2,5 @@ package frc.robot.subsystems.shooter
 
 object ShooterConstants {
 	const val OUTPUT = 0.0
+	const val TIMEOUT_MS = 0
 }
--- src/main/kotlin/frc/robot/subsystems/shooter/ShooterSubsystem.kt
+++ src/main/kotlin/frc/robot/subsystems/shooter/ShooterSubsystem.kt
@@ -1,23 +1,31 @@
 package frc.robot.subsystems.shooter
 
+import com.ctre.phoenix.motorcontrol.ControlMode
+import com.ctre.phoenix.motorcontrol.RemoteFeedbackDevice
+import com.ctre.phoenix.motorcontrol.TalonSRXFeedbackDevice
 import com.ctre.phoenix.sensors.CANCoder
 import com.hamosad1657.lib.motors.HaTalonSRX
+import com.hamosad1657.lib.units.AngularVelocity
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.InstantCommand
 import edu.wpi.first.wpilibj2.command.RunCommand
 import edu.wpi.first.wpilibj2.command.Subsystem
+import edu.wpi.first.wpilibj2.command.SubsystemBase
 import frc.robot.RobotMap
 
-object ShooterSubsystem: Subsystem {
+object ShooterSubsystem: SubsystemBase() {
 	private val motor1 = HaTalonSRX(RobotMap.Shooter.HATALONSRX_ID1)
 	private val motor2 = HaTalonSRX(RobotMap.Shooter.HATALONSRX_ID2)
 	private val encoder1 = CANCoder(RobotMap.Shooter.CANCODER_ID1)
 	private val encoder2 = CANCoder(RobotMap.Shooter.CANCODER2_ID)
 
-	fun shootBallsCommand(): Command {
-	return InstantCommand({RunCommand({motor1.set(ShooterConstants.OUTPUT)}, this)})
-	}
 	init {
+		motor1.configRemoteFeedbackFilter(encoder1, 0, ShooterConstants.TIMEOUT_MS)
+		motor1.configSelectedFeedbackSensor(RemoteFeedbackDevice.RemoteSensor0)
 		motor2.follow(motor1)
+	}
+
+	fun shootBallsCommand(angularVelocity: AngularVelocity): Command {
+	return RunCommand({motor1.set(ControlMode.Velocity, angularVelocity.degPs)}, this).finallyDo { motor1.set(0.0) }
 	}
 }
