06e50fe742735c955ee5674612b8ce62263f769f
Hamosad 1657 <hamosadr@gmail.com>
2023-11-23 18:45:03 +0200

    Hagar and Shaked: write Hood getToAngleCommand

--- src/main/kotlin/frc/robot/subsystems/hood/HoodConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/hood/HoodConstants.kt
@@ -3,4 +3,8 @@ package frc.robot.subsystems.hood
 object HoodConstants {
 	const val GEAR_RATIO_ENCODER_TO_HOOD = 0.0
 	const val MAX_HOOD_ANGLE = 180.0
+
+	const val kP = 0.0
+	const val kI = 0.0
+	const val kD = 0.0
 }
--- src/main/kotlin/frc/robot/subsystems/hood/HoodSubsystem.kt
+++ src/main/kotlin/frc/robot/subsystems/hood/HoodSubsystem.kt
@@ -2,26 +2,38 @@ package frc.robot.subsystems.hood
 
 import com.ctre.phoenix.sensors.CANCoder
 import com.hamosad1657.lib.motors.HaTalonFX
+import edu.wpi.first.math.controller.PIDController
 import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.wpilibj.DigitalInput
+import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.RunCommand
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import frc.robot.RobotMap
+import frc.robot.subsystems.hood.HoodConstants.kD
+import frc.robot.subsystems.hood.HoodConstants.kI
+import frc.robot.subsystems.hood.HoodConstants.kP
 
 object HoodSubsystem : SubsystemBase() {
 	private val motor = HaTalonFX(RobotMap.Hood.MOTOR_ID)
 	private val encoder = CANCoder(RobotMap.Hood.ENCODER_ID)
 
+	private val hoodController = PIDController(kP, kI, kD)
 
 	private val reverseLimitSwitch = DigitalInput(RobotMap.Hood.LIMIT_CHANNEL)
 
 	init {
 		motor.forwardLimit = { hoodAngle.degrees >= HoodConstants.MAX_HOOD_ANGLE }
-
 		motor.reverseLimit = { reverseLimitSwitch.get() }
-
 	}
+
+	fun getToAngleCommand(desiredAngleSupplier: () -> Rotation2d): Command {
+		return RunCommand({
+			val output = hoodController.calculate(hoodAngle.degrees, desiredAngleSupplier().degrees)
+			motor.set(output)
+		}).finallyDo { motor.set(0.0) }
+	}
+
 
 	val hoodAngle: Rotation2d
 		get() = Rotation2d.fromDegrees(encoder.position * HoodConstants.GEAR_RATIO_ENCODER_TO_HOOD)
-
 }
