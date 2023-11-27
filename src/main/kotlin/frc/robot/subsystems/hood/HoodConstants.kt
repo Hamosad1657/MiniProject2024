a10085928bb9b2df3b62a341a7e18da74554900b
Hamosad 1657 <hamosadr@gmail.com>
2023-11-27 18:34:31 +0200

    Hagar & Shaked: renamed constant.

--- src/main/kotlin/frc/robot/subsystems/hood/HoodConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/hood/HoodConstants.kt
@@ -4,7 +4,7 @@ import com.hamosad1657.lib.math.PIDGains
 
 object HoodConstants {
 	const val GEAR_RATIO_ENCODER_TO_HOOD = 0.0
-	const val MAX_HOOD_ANGLE = 180.0
+	const val MAX_HOOD_ANGLE_DEG = 180.0
 
 	val PID_GAINS = PIDGains(0.0, 0.0, 0.0)
 }
