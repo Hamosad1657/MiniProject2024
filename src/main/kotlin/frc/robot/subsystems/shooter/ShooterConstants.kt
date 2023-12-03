891568ebd74e3b2e0450e2e6a0e72217d77885f3
Hamosad 1657 <hamosadr@gmail.com>
2023-12-03 20:51:26 +0200

    Sufi: Added shooting calculations constants

--- src/main/kotlin/frc/robot/subsystems/shooter/ShooterConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/shooter/ShooterConstants.kt
@@ -1,10 +1,18 @@
 package frc.robot.subsystems.shooter
 
 import com.hamosad1657.lib.math.PIDGains
+import com.hamosad1657.lib.units.centimeters
 
 object ShooterConstants {
 	val PID_GAINS = PIDGains(0.0, 0.0, 0.0)
 
 	const val SHOOTER_BALLS_PER_ROTATION = 0.0
 
+	object Calculations {
+		const val BOILER_Y = 0.0
+		const val TURRET_Y = 0.0
+		const val MAX_HEIGHT = 0.0
+		val WHEEL_RADIUS = 20.centimeters
+		const val G = 9.81
+	}
 }
