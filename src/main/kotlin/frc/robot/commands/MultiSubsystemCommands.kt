98e174c152d3af0e9d0bec2f9bde295c61cc67c6
Hamosad 1657 <hamosadr@gmail.com>
2023-12-10 15:59:23 +0200

    Hagar: making a multi command (unfinished)
    It sets the conveyor when hood, shooter and turret are all in tolerance

--- src/main/kotlin/frc/robot/commands/MultiSubsystemCommands.kt
+++ src/main/kotlin/frc/robot/commands/MultiSubsystemCommands.kt
@@ -6,6 +6,7 @@ import edu.wpi.first.math.geometry.Pose2d
 import edu.wpi.first.wpilibj2.command.Command
 import frc.robot.mechanisms.BOILER_LOCATION
 import frc.robot.mechanisms.HoodShooterState
+import frc.robot.subsystems.conveyor.ConveyorSubsystem
 import frc.robot.subsystems.hood.HoodSubsystem
 import frc.robot.subsystems.shooter.ShooterSubsystem
 import frc.robot.subsystems.turret.TurretSubsystem
@@ -31,4 +32,11 @@ fun getToStateCommand(stateSupplier: () -> HoodShooterState): Command {
 			ShooterSubsystem.stopShooter()
 			HoodSubsystem.stopCommand()
 		}
+}
+
+fun setConveyorWhenHoodShooterAndTurretInToleranceCommand(): Command {
+	val getToStateWithTurretCommand = getToStateCommand { }.alongWith(TurretSubsystem.getToAngleCommand())
+	return if (TurretSubsystem.withinTolerance() && HoodSubsystem.withinTolerance() && ShooterSubsystem.withinTolerance()) {
+		getToStateWithTurretCommand.alongWith(ConveyorSubsystem.loadBallsCommand())
+	} else getToStateWithTurretCommand
 }
