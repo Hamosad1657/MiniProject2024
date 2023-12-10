5c12e01f60e4bef2dbdbcf26f51b3a6931edec6e
Hamosad 1657 <hamosadr@gmail.com>
2023-12-10 10:53:25 +0200

    Hagar & Shaked: Added toggle intake command to driver B circle button

--- src/main/kotlin/frc/robot/RobotContainer.kt
+++ src/main/kotlin/frc/robot/RobotContainer.kt
@@ -2,6 +2,9 @@ package frc.robot
 
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+import frc.robot.commands.collectCommand
+import frc.robot.commands.stopIntakeCommand
+import frc.robot.subsystems.intake.IntakeSubsystem
 
 object RobotContainer {
 	private val controllerA = CommandPS4Controller(RobotMap.DRIVER_A_CONTROLLER_PORT)
@@ -14,7 +17,11 @@ object RobotContainer {
 
 	private fun configureBindings() {}
 
-	private fun setDefaultCommands() {}
+	private fun setDefaultCommands() {
+		// Toggle intake on circle button
+		controllerB.circle()
+			.toggleOnTrue(IntakeSubsystem.collectCommand().finallyDo { IntakeSubsystem.stopIntakeCommand() })
+	}
 
 	fun getAutonomousCommand(): Command? {
 		return null
