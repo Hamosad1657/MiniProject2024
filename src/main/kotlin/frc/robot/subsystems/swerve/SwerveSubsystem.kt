9277f86cf3f58562408319c6d59fcace5394833d
Hamosad 1657 <hamosadr@gmail.com>
2023-12-14 14:28:35 +0200

    Shaked & Hagar: Added fake vision and swerve

---
+++ src/main/kotlin/frc/robot/subsystems/swerve/SwerveSubsystem.kt
@@ -0,0 +1,8 @@
+package frc.robot.subsystems.swerve
+
+import com.hamosad1657.lib.units.degrees
+import edu.wpi.first.math.geometry.Pose2d
+
+object SwerveSubsystem {
+	val pose = Pose2d(0.0, 0.0, 0.0.degrees)
+}
---
+++ src/main/kotlin/frc/robot/subsystems/vision/Vision.kt
@@ -0,0 +1,53 @@
+package frc.robot.subsystems.vision
+
+import edu.wpi.first.apriltag.AprilTag
+import edu.wpi.first.apriltag.AprilTagFieldLayout
+import edu.wpi.first.math.geometry.*
+import org.photonvision.*
+import org.photonvision.targeting.PhotonTrackedTarget
+
+object Vision {
+
+	val currentBestTag: PhotonTrackedTarget? = null
+
+	val aprilTags: AprilTagFieldLayout =
+		AprilTagFieldLayout(
+			mutableListOf(
+				AprilTag(
+					0,
+					Pose3d(
+						0.0, 0.0, 0.0,
+						Rotation3d(0.0, 0.0, 0.0)
+					)
+				),
+				AprilTag(
+					0,
+					Pose3d(
+						0.0, 0.0, 0.0,
+						Rotation3d(0.0, 0.0, 0.0)
+					)
+				)
+			), 0.0, 0.0
+		)
+
+	private val robotToCamera = Transform3d(Translation3d(0.0, 0.0, 0.0), Rotation3d(0.0, 0.0, 0.0))
+
+	private val camera = PhotonCamera("")
+
+	private val poseEstimator =
+		PhotonPoseEstimator(
+			aprilTags,
+			PhotonPoseEstimator.PoseStrategy.MULTI_TAG_PNP,
+			camera,
+			robotToCamera,
+		).apply {
+			setMultiTagFallbackStrategy(PhotonPoseEstimator.PoseStrategy.LOWEST_AMBIGUITY)
+		}
+
+	/**
+	 * Gets the estimated robot position from the PhotonVision camera.
+	 * Returns null if it doesn't detect any April Tags.
+	 */
+	var estimatedGlobalPose: EstimatedRobotPose? = poseEstimator.update().orElse(null)
+
+}
