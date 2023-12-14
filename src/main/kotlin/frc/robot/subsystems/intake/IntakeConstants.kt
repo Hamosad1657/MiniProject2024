fd4c2b5d1e0bdff137641bd863e4321033a6d226
Hamosad 1657 <hamosadr@gmail.com>
2023-12-14 09:35:00 +0200

    Write unit tests for intake

--- src/main/kotlin/frc/robot/subsystems/intake/IntakeConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/IntakeConstants.kt
@@ -1,5 +1,5 @@
 package frc.robot.subsystems.intake
 
 object IntakeConstants {
-	const val MOTOR_OUTPUT = 0.0
+	const val MOTOR_OUTPUT = 0.5
 }
---
+++ src/test/java/frc/robot/subsystems/intake/IntakeTest.kt
@@ -0,0 +1,37 @@
+package frc.robot.subsystems.intake
+
+import edu.wpi.first.hal.HAL
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.*
+
+/**
+ * Example test just to learn how this works.
+ */
+
+class IntakeTest {
+	val DELTA = 1e-2 // Copied from WPILib's example
+
+	@BeforeEach
+	fun setup() {
+		HAL.initialize(500, 0)
+	}
+
+	@AfterEach
+	fun cleanup() {
+		IntakeSubsystem.close()
+	}
+
+	@Test
+	fun collects() {
+		IntakeSubsystem.collect()
+		assertTrue(IntakeSubsystem.running)
+		assertEquals(IntakeConstants.MOTOR_OUTPUT, IntakeSubsystem.currentOutput, DELTA)
+	}
+
+	@Test
+	fun stops() {
+		IntakeSubsystem.stop()
+		assertFalse(IntakeSubsystem.running)
+		assertEquals(0.0, IntakeSubsystem.currentOutput, DELTA)
+	}
+}
