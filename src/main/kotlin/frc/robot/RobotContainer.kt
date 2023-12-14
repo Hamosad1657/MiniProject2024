52f3451c31395f024ceb249fe100f1f813351be0
Hamosad 1657 <hamosadr@gmail.com>
2023-12-14 14:29:05 +0200

    Hagar and Shaked: Added some button bindings

--- src/main/kotlin/frc/robot/RobotContainer.kt
+++ src/main/kotlin/frc/robot/RobotContainer.kt
@@ -1,26 +1,53 @@
 package frc.robot
 
+import com.hamosad1657.lib.math.simpleDeadband
+import edu.wpi.first.wpilibj.PS4Controller
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-import frc.robot.commands.collectCommand
-import frc.robot.commands.stopIntakeCommand
+import edu.wpi.first.wpilibj2.command.button.Trigger
+import frc.robot.commands.*
 import frc.robot.subsystems.intake.IntakeSubsystem
+import frc.robot.subsystems.shooter.ShooterSubsystem
+import frc.robot.subsystems.swerve.SwerveSubsystem
+import frc.robot.subsystems.turret.TurretSubsystem
+import frc.robot.subsystems.vision.Vision
 
 object RobotContainer {
-	private val controllerA = CommandPS4Controller(RobotMap.DRIVER_A_CONTROLLER_PORT)
-	private val controllerB = CommandPS4Controller(RobotMap.DRIVER_B_CONTROLLER_PORT)
+	const val JOYSTICK_DEADBAND = 0.1
+
+	private val commandControllerA = CommandPS4Controller(RobotMap.DRIVER_A_CONTROLLER_PORT)
+	private val commandControllerB = CommandPS4Controller(RobotMap.DRIVER_B_CONTROLLER_PORT)
+	private val controllerB = PS4Controller(RobotMap.DRIVER_B_CONTROLLER_PORT)
+
+	private val turretTeleopTrigger = Trigger { controllerB.r3Button }
 
 	init {
+		ShooterSubsystem
 		configureBindings()
 		setDefaultCommands()
 	}
 
-	private fun configureBindings() {}
+	private fun configureBindings() {
+		// Toggle intake on circle button
+		commandControllerB.circle()
+			.toggleOnTrue(IntakeSubsystem.collectCommand().finallyDo { IntakeSubsystem.stopIntakeCommand() })
+
+		// Toggle aim shoot and load on cross button
+		commandControllerB.cross().toggleOnTrue(aimAndLoadWhenAimedCommand { SwerveSubsystem.pose })
+
+		// Toggle turret teleop control on right joystick press. Default command runs otherwise
+		turretTeleopTrigger.toggleOnTrue(
+			TurretSubsystem.closedLoopTeleopCommand(
+				{ simpleDeadband(commandControllerB.rightX, JOYSTICK_DEADBAND) },
+				{ simpleDeadband(-commandControllerB.rightX, JOYSTICK_DEADBAND) })
+		)
+
+		commandControllerB.options().onTrue(TurretSubsystem.searchForAnyTagCommand { Vision.currentBestTag })
+
+	}
 
 	private fun setDefaultCommands() {
-		// Toggle intake on circle button
-		controllerB.circle()
-			.toggleOnTrue(IntakeSubsystem.collectCommand().finallyDo { IntakeSubsystem.stopIntakeCommand() })
+		TurretSubsystem.defaultCommand = TurretSubsystem.aimTurretCommand { SwerveSubsystem.pose }
 	}
 
 	fun getAutonomousCommand(): Command? {
