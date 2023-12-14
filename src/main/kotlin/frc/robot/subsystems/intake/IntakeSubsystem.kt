4c8e92f22a26f1c9155a880b6c4420b87bd7ccb5
Hamosad 1657 <hamosadr@gmail.com>
2023-12-14 11:50:10 +0200

    Shaked: Added IntakeSubsystem to dashboard

--- src/main/kotlin/frc/robot/subsystems/intake/IntakeSubsystem.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/IntakeSubsystem.kt
@@ -2,11 +2,13 @@ package frc.robot.subsystems.intake
 
 import com.hamosad1657.lib.motors.HaTalonFX
 import edu.wpi.first.util.sendable.SendableBuilder
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import frc.robot.RobotMap
 import frc.robot.subsystems.intake.IntakeConstants as Constants
 
 object IntakeSubsystem : SubsystemBase(), AutoCloseable {
+
 	private val motor = HaTalonFX(RobotMap.Intake.MOTOR_ID)
 
 	fun collect() = motor.set(Constants.MOTOR_OUTPUT)
@@ -14,6 +16,10 @@ object IntakeSubsystem : SubsystemBase(), AutoCloseable {
 
 	val running: Boolean
 		get() = motor.get() != 0.0
+
+	init {
+		SmartDashboard.putData("Intake", this)
+	}
 
 	override fun initSendable(builder: SendableBuilder) {
 		builder.addBooleanProperty("Running", { running }, null)
