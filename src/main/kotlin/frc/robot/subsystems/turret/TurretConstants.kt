09521ad92ff92eddd0e10bc21614fd38c9de657e
Shaked <shakedshibo@gmail.com>
2023-12-16 17:02:49 +0200

    Shaked: Add comments in TurretConstants

--- src/main/kotlin/frc/robot/subsystems/turret/TurretConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/turret/TurretConstants.kt
@@ -8,8 +8,16 @@ object TurretConstants {
 
 	const val TOLERANCE_DEGREES = 0.0
 
+	/**
+	 * Minimum angle of the turret. This may be different from
+	 * what is measured by the CANCoder, due to the gear ratio.
+	 */
 	val MIN_ANGLE = 0.0.degrees
-	val MAX_ANGLE = 350.0.degrees
+	/**
+	 * Maximum angle of the turret. This may be different from
+	 * what is measured by the CANCoder, due to the gear ratio.
+	 */
+	val MAX_ANGLE = 350.0.degrees // TODO: Change to precise value
 
 	const val SCAN_FOR_TAGS_ROTATION_OUTPUT = 0.0
 
@@ -20,7 +28,15 @@ object TurretConstants {
 	 * it should return the turret's angle in 0 to 360.
 	 */
 	const val GEAR_RATIO_ENCODER_TO_TURRET = 0.0
+
 	const val TELEOP_ANGLE_MULTIPLIER = 0.0
 
 	const val TAG_DETECTION_TIME_SEC = 0.5
+	/**
+	 * This value should be based on how long PhotonVision detects false-positives for.
+	 *
+	 * The robot code has a loop time of 20 milliseconds. So for example, a detection
+	 * time of 500 milliseconds means the tag has to be detected for 25 loops in a row
+	 * to count.
+	 */
 }
