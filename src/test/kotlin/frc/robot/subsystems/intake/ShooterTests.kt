55848f1ef3ded014b9c364fba2b4d465f090e0ed
Hamosad 1657 <hamosadr@gmail.com>
2023-12-19 15:19:07 +0200

    Shaked: Refactored shootingSpeedTest to use assertEquals

--- src/test/kotlin/frc/robot/subsystems/intake/ShooterTests.kt
+++ src/test/kotlin/frc/robot/subsystems/intake/ShooterTests.kt
@@ -1,17 +1,19 @@
 package frc.robot.subsystems.intake
 
-import com.hamosad1657.lib.math.roundWithPrecision
 import com.hamosad1657.lib.units.Length
 import frc.robot.mechanisms.HoodShooterState
 import frc.robot.subsystems.shooter.ShooterConstants.Calculations
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+
+const val DELTA = 0.0001
 
 class ShooterTests {
 	@Test
 	fun shootingSpeedTest() {
 		HoodShooterState.fromDistance(Length.fromMeters(4.0)).let {
-			assert(it.hoodAngle.radians.roundWithPrecision(4) == 1.0202)
-			assert(it.angularVelocity.toMps(Calculations.WHEEL_RADIUS).roundWithPrecision(4) == 7.8636)
+			assertEquals(1.0202, it.hoodAngle.radians, DELTA)
+			assertEquals(7.8636, it.angularVelocity.toMps(Calculations.WHEEL_RADIUS), DELTA)
 		}
 	}
 }
