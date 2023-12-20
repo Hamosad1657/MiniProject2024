8a262a6b0bb2b20f6fe91d82f9da54058a0bf16a
Hamosad 1657 <hamosadr@gmail.com>
2023-12-20 16:56:30 +0200

    Hagar & Shaked: Renamed turret limit port to limit channel

--- src/main/kotlin/frc/robot/subsystems/turret/TurretSubsystem.kt
+++ src/main/kotlin/frc/robot/subsystems/turret/TurretSubsystem.kt
@@ -33,8 +33,8 @@ object TurretSubsystem : SubsystemBase() {
 		configAbsoluteSensorRange(AbsoluteSensorRange.Unsigned_0_to_360)
 	}
 
-	private val CWLimit = DigitalInput(RobotMap.Turret.CW_LIMIT_PORT)
-	private val CCWLimit = DigitalInput(RobotMap.Turret.CCW_LIMIT_PORT)
+	private val CWLimit = DigitalInput(RobotMap.Turret.CW_LIMIT_CHANNEL)
+	private val CCWLimit = DigitalInput(RobotMap.Turret.CCW_LIMIT_CHANNEL)
 
 	/** CCW positive, according to standard mathematical conventions (and WPILib). */
 	val currentAngle: Rotation2d get() = Rotation2d.fromDegrees(encoder.position * Constants.GEAR_RATIO_ENCODER_TO_TURRET)
