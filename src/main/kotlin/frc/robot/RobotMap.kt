c8fd1176c53f86c33f24e5500061b91711da1c49
Hamosad 1657 <hamosadr@gmail.com>
2023-12-23 11:04:18 +0200

    Hagar & Shaked: Set CAN IDs and channel numbers

--- src/main/kotlin/frc/robot/RobotMap.kt
+++ src/main/kotlin/frc/robot/RobotMap.kt
@@ -5,29 +5,29 @@ object RobotMap {
 	const val DRIVER_B_CONTROLLER_PORT = 1
 
 	object Turret {
-		const val MOTOR_ID = 0
-		const val CANCODER_ID = 0
-		const val CW_LIMIT_CHANNEL = 0
-		const val CCW_LIMIT_CHANNEL = 1
+		const val MOTOR_ID = 4
+		const val CANCODER_ID = 3
+		const val CW_LIMIT_CHANNEL = 1
+		const val CCW_LIMIT_CHANNEL = 2
 	}
 
 	object Shooter {
-		const val MOTOR_ID = 0
+		const val MOTOR_ID = 7
 	}
 
 	object Hood {
-		const val MOTOR_ID = 0
+		const val MOTOR_ID = 6
+		const val ENCODER_ID = 5
 		const val BOTTOM_LIMIT_CHANNEL = 0
 		const val TOP_LIMIT_CHANNEL = 0
-		const val ENCODER_ID = 0
 	}
 
 	object Conveyor {
-		const val CONVEYOR_MOTOR_ID = 0
-		const val LOADER_MOTOR_ID = 0
+		const val CONVEYOR_MOTOR_ID = 10
+		const val LOADER_MOTOR_ID = 9
 	}
 
 	object Intake {
-		const val MOTOR_ID = 0
+		const val MOTOR_ID = 8
 	}
 }
